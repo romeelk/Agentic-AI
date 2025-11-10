361a8a41c4f515d37cea8a3ad18817ca40aae1f8
romeelk <romeel_k@yahoo.co.uk>
2025-11-10 22:04:05 +0000

    Added deploy agent

--- src/adk_agent/agent.py
+++ src/adk_agent/agent.py
@@ -1,33 +1,23 @@
-# from google.adk.agents.llm_agent import Agent
-# import datetime
-
-# tools = []
-
-# def get_utc_time():
-#     return datetime.datetime.now(datetime.UTC).strftime("%c")
-
-# def get_tools():
-#     return [func.__name__ for func in tools if func.__name__ != "get_tools"]
-
-# tools = []
-# tools.append(get_utc_time)
-# # tools.append(get_weather)
-# tools.append(get_tools)
-
-# root_agent = Agent(
-#     model='gemini-2.5-flash',
-#     name='root_agent',
-#     description='A helpful assistant for user questions.',
-#     instruction='Answer user questions to the best of your knowledge',
-#     tools=tools
-# )
-
 from google.adk.agents.llm_agent import Agent
+import datetime
+
+tools = []
+
+def get_utc_time():
+    return datetime.datetime.now(datetime.UTC).strftime("%c")
+
+def get_tools():
+    return [func.__name__ for func in tools if func.__name__ != "get_tools"]
+
+tools = []
+tools.append(get_utc_time)
+tools.append(get_tools)
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
+    tools=tools
 )
 
