65a654d92680859523b376627d70e2e14a848e97
romeelk <romeel_k@yahoo.co.uk>
2026-02-04 22:26:41 +0000

    Added AI foundry latest sdk version

--- src/ms-foundry-expense-agent/src/foundry-agent.py
+++ src/ms-foundry-expense-agent/src/foundry-agent.py
@@ -10,6 +10,7 @@ foundry_endpoint = os.getenv("AZURE_EXISTING_AIPROJECT_ENDPOINT")
 
 project_client = AIProjectClient(endpoint=foundry_endpoint, credential=DefaultAzureCredential())
 
+
 # name of existing agent
 foundry_agent_name = "expense-agent"
 
@@ -19,11 +20,21 @@ if foundry_agent is not None:
     print(f"Fetched existing {foundry_agent.name}")
 
     openai_client = project_client.get_openai_client()
-    response = openai_client.responses.create(
-    input=[{"role": "user", "content": "Tell me what you can help with."}],
+
+    # replaces previous API thread concept
+    conversation = openai_client.conversations.create(
+            items=[{"type": "message", "role": "user", "content": "Tell me what you can help with?"}],
+        )
+    response = openai_client.responses.create(conversation=conversation.id,
     extra_body={"agent": {"name": foundry_agent.name, "type": "agent_reference"}},
     )
 
     print(f"Response output: {response.output_text}")
 
+    response = openai_client.responses.create(
+    conversation=conversation.id,
+    extra_body={"agent": {"name": foundry_agent.name, "type": "agent_reference"}},
+    input="And what is expense limit for category Travel?",
+)
+    print(f"Response output: {response.output_text}")
 
