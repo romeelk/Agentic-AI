d8527b2a70df17f65347b3fd86a521b519e2da77
romeelk <romeel_k@yahoo.co.uk>
2026-08-04 16:53:52 +0100

    Added google search app data store for rag lookup'

--- src/adk_advanced_rag/advanced_rag/agent.py
+++ src/adk_advanced_rag/advanced_rag/agent.py
@@ -1,8 +1,29 @@
-from google.adk.agents.llm_agent import Agent
+from google.adk.agents.llm_agent import Agent, LlmAgent
 from google.adk.tools import FunctionTool
+from google.adk.tools import VertexAiSearchTool
+from google.adk.tools import AgentTool
 import pandas as pd
 from datetime import datetime
 import os
+
+
+# 1. Create the Vertex AI Search tool
+full_datastore_id = f"projects/{os.environ['GOOGLE_CLOUD_PROJECT']}/locations/global/collections/default_collection/dataStores/{os.environ['DATA_STORE_ID']}"
+vertex_ai_search_tool = VertexAiSearchTool(
+    data_store_id=full_datastore_id
+)
+
+doc_qa_agent = LlmAgent(
+    name="search_and_qna_agent",
+    model="gemini-2.5-flash",
+    tools=[vertex_ai_search_tool],
+    instruction="""You are a helpful assistant that answers questions based on information found in the document store.
+    Use the search tool to find relevant information before answering.
+    If the answer isn't in the documents, say that you couldn't find the information.
+    """,
+    description="Answers questions using a specific Vertex AI Search datastore.",
+)
+
 
 def convert_dataframe_to_json(data:pd.DataFrame):
     return  {
@@ -31,13 +52,13 @@ def load_msft_ticker_data(query_date:str):
     except Exception as e:
         return {"status": "error", "message": f"An error occurred: {str(e)}"}
     
-root_agent = Agent(
+root_agent = LlmAgent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
-    tools=[load_msft_ticker_data],
+    tools=[AgentTool(doc_qa_agent),load_msft_ticker_data],
     instruction="""Answer user questions about Microsofts stock prices. You have been provided a file via the tool load_msft_ticker_data that lists the last two weeks of July stock listsings.' \
-    If you cannot find the stock listings given a particular date, then respond That you have only data  29th June 2026 upto including 15th July 2026"
+    If you cannot find the stock listings given a particular date, then respond That you have only data  29th June 2026 upto including 15th July 2026. You can also use doc_qa_agent tool to lookup microsoft earnings report info "
     """
 )
 
--- src/adk_advanced_rag/advanced_rag/readme.md
+++ src/adk_advanced_rag/advanced_rag/readme.md
@@ -3,6 +3,9 @@
 This agent example demonstrates the ability to use agent as a tool
 to decide on what RAG source to use in response to the user query.
 
+It is based on the google tutorial:
+https://codelabs.developers.google.com/codelabs/production-ready-ai-with-gc/7-advanced-agent-capabilities/building-agents-with-retrieval-augmented-generation#0
+
 It has two sources of grounding:
 - Microsoft q2 report via web url:https://www.microsoft.com/en-us/investor/earnings/fy-2026-q2/press-release-webcast
 - Structured csv file of the last two weeks of stock prices starting and closing prices
