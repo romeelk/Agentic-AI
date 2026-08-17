f6b3233cec54ead1c995fb6858d577c95b92ec9b
romeelk <romeel_k@yahoo.co.uk>
2026-08-17 16:37:02 +0100

    Fixed adk agent errors

--- src/adk_basic_agent/gettimezone_agent/agent.py
+++ src/adk_basic_agent/gettimezone_agent/agent.py
@@ -4,7 +4,7 @@ from google.adk.tools import FunctionTool
 import datetime
 import pytz
 
-tools = []
+time_toolstools = []
 
 [FunctionTool]
 def get_utc_time():
@@ -21,19 +21,19 @@ def get_country_time_zone(country_timezone_name):
     return datetime.datetime.now(country_time_zone).strftime("%c")
 
 def get_tools():
-    return [func.__name__ for func in tools if func.__name__ != "get_tools"]
+    return [func.__name__ for func in time_tools if func.__name__ != "get_tools"]
 
-tools = []
-tools.append(get_utc_time)
-tools.append(get_country_time_zones)
-tools.append(get_country_time_zone)
-tools.append(get_tools)
+time_tools = []
+time_tools.append(get_utc_time)
+time_tools.append(get_country_time_zones)
+time_tools.append(get_country_time_zone)
+time_tools.append(get_tools)
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for providing current time for different world time zones',
-    instruction='Answer user questions relating to current time and time in different time zones. Use available tools get_utc_time and get_country_time_zone',
-    tools=tools
+    instruction='Answer user questions relating to current time and time in different time zones. Use available tools get_utc_time and get_country_time_zone. If asked for a time zone for a Country you must prompt the user to provide the region for that country',
+    tools=time_tools
 )
 
